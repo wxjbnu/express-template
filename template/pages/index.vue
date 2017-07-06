a302909be5866f73af5e90c33600bb2da32f3296
wxj <wxj>
2017-07-06 11:18:36 +0800

    修改模板

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -1,3 +1,4 @@
+{{{{raw}}}}
 <template>
   <section class="container">
     <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" >
@@ -15,7 +16,7 @@
     <a href="javascript:void(0)" @click="show">show</a>
   </section>
 </template>
-
+{{{{/raw}}}}
 <script>
 import axios from '~plugins/axios'
 
